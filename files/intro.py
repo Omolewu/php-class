c5c7822ccf37d161f566cdf9905d82ee047228d6
Omolewu Sunday <omolewu12@gmail.com>
2021-10-26 13:34:43 +0100

    Adding files uploading

---
+++ files/intro.py
@@ -0,0 +1,41 @@
+from random import randint
+# print(2 + 2)
+# print(3 + 2)
+# print(3 + 2)
+# print(3 + 4)
+# a=5
+# if a<3:
+#     print("Hello Python")
+# for i in range(1, 11):
+#     print(i);
+rands =[]
+for i in range(5):
+    rands.append(randint(1, 10))
+  
+print(rands)
+
+# while True:
+#     userrand=int(input("Pick a number")) 
+#     if(randnum == userrand):
+#         print(f"Great you are correct {randnum}")
+#         ans=input("Do you still want to play (y/n) ")
+#         validans=['y', 'n']
+#         if ans not in  validans:
+#             print("Invalid input")
+#             ans=input("Do you still want to play (y/n) ")
+#         else:
+#             if ans =='y':
+#                 randnum = randint(1, 10)
+#                 userrand=int(input("Pick a number"))
+#             else:
+#                 print("Thanks for playing our game \n Goodbye")
+#                 break
+            
+            
+#     else:
+#         print(f"Wrong the correct answer is {randnum}")
+   
+    
+   
+  
+# print("Correct Password")
